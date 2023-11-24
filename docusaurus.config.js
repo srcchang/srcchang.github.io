29d38ad6a29d0ed24f6ccac5b6daa140b214eb90
srcchang <srcchang@gmail.com>
2023-11-24 17:01:13 +0800

    Add github action workflows.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
   projectName: 'srcchang.github.io', // Usually your repo name.
   trailingSlash:false,
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
