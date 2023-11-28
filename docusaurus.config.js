68b69a6d6272443708d4046295d27980b7c4955c
srcchang <srcchang@gmail.com>
2023-11-28 12:36:42 +0800

    Add gtag.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -58,6 +58,10 @@ const config = {
         theme: {
           customCss: './src/css/custom.css',
         },
+        gtag: {
+          trackingID: 'G-DXESCMJLZF',
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
